e2885abc27975741cceac6d22c350297e9d48e13
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-03-08 16:49:30 +0000

    css working

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+const path = require('path');
 
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('home');
